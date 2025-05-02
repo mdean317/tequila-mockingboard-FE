fe5d836ed7e451b6cbe71101113e0c46253d2d15
JohnNg-GA <john.q.ng@gmail.com>
2025-05-02 10:52:39 -0700

    tailwind css feature implemented

--- src/assets/components/Recipes/Recipes.jsx
+++ src/assets/components/Recipes/Recipes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useEffect, useState } from 'react'
-import './Recipes.css'
 import { useNavigate } from 'react-router-dom';
 
 const Recipes = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode, React } from 'react'
 import { createRoot, ReactDOM } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
+import './styles.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
