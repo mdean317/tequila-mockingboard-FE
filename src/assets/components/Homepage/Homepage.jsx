619b9610e11da95ca6d69bd909349e7aaa08a152
Shawnade <deshawnagalloway@yahoo.com>
2025-05-05 19:33:17 -0400

    updated home page css

--- src/assets/components/Homepage/Homepage.jsx
+++ src/assets/components/Homepage/Homepage.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <title>Tequila-Mockingbird</title>
 
       <div>
-        <h1>Welcome to our very own Mixer App!</h1>
+        <h1 className='text-8xl p-5 m-2 bg-teal-700/75 rounded-full'>Welcome to our very own Mixer App!</h1>
         <p>Pick from the very best ingredients in order to create your favorite drinks!</p>
         <p>Easy to navigate, no hassle-- fulfill to your heart's enjoyment!</p>
         <p>Ready to shop? Let's get started!</p>
