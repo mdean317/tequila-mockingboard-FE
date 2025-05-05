20591b0edc62b86f94d72064fb17abe2de6748ce
Shawnade <deshawnagalloway@yahoo.com>
2025-05-05 12:27:35 -0400

    updated userProfile.jsx

--- src/assets/components/NavBar/NavBar.jsx
+++ src/assets/components/NavBar/NavBar.jsx
@@ -5,20 +5,20 @@ const Navbar = ({ user }) => {
   return (
     <nav className="flex justify-around p-4" id="top-navbar"> {/* */}
 
-      <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/">Home</Link>
+      <Link className="text-black font-bold p-2 bg-yellow-50 rounded-xl" to="/">Home</Link>
       {user === true ? (
         <>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/signin">Sign In</Link>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/signup">Sign Up</Link>
+          <Link className="text-black font-bold p-2 bg-white rounded-xl" to="/signin">Sign In</Link>
+          <Link className="text-black font-bold p-2 bg-white rounded-xl" to="/signup">Sign Up</Link>
           
         </>
       ) : (
         <>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/ingredients/list">Ingredients</Link>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/shoppinglists">Your Shopping Lists</Link>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/recipes">Recipes</Link>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/logout">Log Out</Link>
-          <Link className="text-blue-700 font-bold p-2 bg-green-700 rounded-xl" to="/user" >Profile</Link>
+          <Link className="text-black font-bold p-2 bg-yellow-50 rounded-xl" to="/ingredients/list">Ingredients</Link>
+          <Link className="text-black font-bold p-2 bg-yellow-50 rounded-xl" to="/shoppinglists">Your Shopping Lists</Link>
+          <Link className="text-black font-bold p-2 bg-yellow-50 rounded-xl" to="/recipes">Recipes</Link>
+          <Link className="text-black font-bold p-2 bg-yellow-50 rounded-xl" to="/logout">Log Out</Link>
+          <Link className="text-black font-bold p-2 bg-yellow-50 rounded-xl" to="/user" >Profile</Link>
         </>
       )}
     </nav>
